a032520aff57f61e7a6f0f4f4f72eeac2f1a2e6b
Rout222 <routmagno@gmail.com>
2018-05-23 12:54:24 -0300

    criando rede MLP

--- classificar.R
+++ classificar.R
@@ -1,6 +1,6 @@
 #limpar workspace
 rm(list=ls())
-
+library("RSNNS")
 #limpar tela
 cat('\014')
 dados <- read.table(
@@ -27,6 +27,8 @@ gerarGraficos <- function(df,nome){
 x <- dados[,-5] # retira a classificação
 y <- dados$Classe # classificaçao
 
+
+#gerar os gráficos
 col <- c("DistanciaMaiorDefeito","AreaMinElipse","AreaMinRec","AreaMinCircle")
 
 for (i in seq(1,length(x))) {
@@ -35,3 +37,30 @@ for (i in seq(1,length(x))) {
   gerarGraficos(df, col[i])  
 }
 
+indicesDeTreino = NULL
+indicesDeTeste1 = NULL
+indicesDeTeste2 = NULL
+
+#separa os indices em 3 grupos, de 60%, 20%, 20%, por tipo
+for (i in seq(1,3)) {
+  indices = which(y==(i-1))
+  indices = sample(indices)
+  size    = length(indices)
+  treino = 1:(floor(0.6*size))
+  teste1 = (floor(0.6*size)+1):(floor(0.8*size))
+  teste2 = (floor(0.8*size)+1):size
+  indicesDeTreino = c(indicesDeTreino, indices[treino])
+  indicesDeTeste1 = c(indicesDeTeste1, indices[teste1])
+  indicesDeTeste2 = c(indicesDeTeste2, indices[teste2])
+}
+
+nNeuronios = 20
+maxEpocas  = 30000
+RedeCA<-mlp(x[indicesDeTreino,], y[indicesDeTreino], size=nNeuronios, maxit=maxEpocas, initFunc="Randomize_Weights",
+            initFuncParams=c(-0.3, 0.3), learnFunc="Std_Backpropagation",
+            learnFuncParams=c(0.051), updateFunc="Topological_Order",
+            updateFuncParams=c(0), hiddenActFunc="Act_Logistic",
+            shufflePatterns=F, linOut=TRUE)
+
+plot(RedeCA$IterativeFitError,type="l",main="Erro da MLP CA")
+print(paste( "Erro quadrado médio do treino, " ,mean(sqrt((y-predict(RedeCA, x))^2))))
